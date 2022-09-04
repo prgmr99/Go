5d0fde1f5fb1917339a6b1ca87771220b900264a
prgmr99 <johnyeom24@naver.com>
2022-09-04 15:13:02 +0900

    [Add] Handling Errors

--- Projects/banking.go
+++ Projects/banking.go
@@ -1,10 +1,14 @@
 package accounts
 
+import "errors"
+
 // Account struct
 type Account struct {
 	owner   string // 소문자는 private
 	balance int    // 대문자는 public, 함수도 마찬가지.
 }
+
+var errNoMoney = errors.New("can't withdraw") // error를 만들 떄, 이름을 err를 붙이는 것이 좋다.
 
 // NewAccount creates Account
 func NewAccount(owner string) *Account {
@@ -13,9 +17,11 @@ func NewAccount(owner string) *Account {
 }
 
 // Deporsit x amount on your account (Method 작성하는 법)
-func (a Account) Deposit(amount int) { // receiver이라고 부른다. a의 type은 Account이다. 이름은 자유롭게 지을 수 있다.
+func (a *Account) Deposit(amount int) { // receiver이라고 부른다. a의 type은 Account이다. 이름은 자유롭게 지을 수 있다.
 	a.balance += amount
 }
+
+// 단순히 a Account로 작성하면 복사본을 받기 때문에 원본의 값에는 영향을 미치지 않는다. -> a *Account로 수정.
 
 // receiver을 작성하는 데 주의해야할 점
 // struct의 첫 글자를 따서 소문자로 지어야 한다는 것.
@@ -24,3 +30,16 @@ func (a Account) Deposit(amount int) { // receiver이라고 부른다. a의 type
 func (a Account) Balance() int {
 	return a.balance
 }
+
+// Withdraw from your account
+func (a *Account) Withdraw(amount int) error {
+	if a.balance < amount {
+		return errNoMoney
+	}
+
+	a.balance -= amount
+	return nil // Null or None 과 같은 것.
+}
+
+// Go에는 exception이 없다. try~except / try~catch 등.
+// error을 return해줘야 하고 error를 직접 체크해줘야한다.
--- Projects/main1.go
+++ Projects/main1.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"fmt"
-
-	accounts "github.com/prgmr99/learngo/banking"
+	"log"
 )
 
 func main() {
-	account := accounts.NewAccount("john")
+	account := accounts.NewAccount("john") //accounts.NewAccount("john")
 	account.Deposit(10)
+	fmt.Println(account.Balance())
+
+	// Go에서 error를 handling하는 법.
+	err := account.Withdraw(20)
+	if err != nil {
+		log.Fatalln(err) // Println()을 호출하고 프로그램을 종료시킨다. fmt.Println(err)로 작성하면 can't withdraw만 바로 나온다.
+	}
 	fmt.Println(account.Balance())
 }
